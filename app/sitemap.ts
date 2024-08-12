e177bb9591dbd1c3276e0ee1b5dd39956c13dbfb
KrzysiekDeweloperskie <krzysiek.kutniowski@deweloperskie.pl>
2024-08-12 14:37:18 +0200

    implement sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,30 @@
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "https://wnetrza.deweloperskie.pl",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 1,
+    },
+    {
+      url: "https://wnetrza.deweloperskie.pl/informacje",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.5,
+    },
+    {
+      url: "https://wnetrza.deweloperskie.pl/sokolska-towers-126",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.9,
+    },
+    // {
+    //   url: 'https://acme.com/blog',
+    //   lastModified: new Date(),
+    //   changeFrequency: 'weekly',
+    //   priority: 0.5,
+    // },
+  ];
+}
