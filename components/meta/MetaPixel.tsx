78937257e523518c4da373603d6d6888e06e53a6
KrzysiekDeweloperskie <krzysiek.kutniowski@deweloperskie.pl>
2024-08-19 13:05:14 +0200

    Meta Pixel test 4

--- components/meta/MetaPixel.tsx
+++ components/meta/MetaPixel.tsx
@@ -1,21 +1,40 @@
-"use client";
-import React, { useEffect } from "react";
-import { usePathname, useSearchParams } from "next/navigation";
+/* eslint-disable @next/next/no-img-element */
+// components/FacebookPixel.tsx
+import Script from "next/script";
+import React from "react";
 
-const MetaPixel: React.FC = () => {
-  const pathname = usePathname();
-  const searchParams = useSearchParams();
-
-  useEffect(() => {
-    import("react-facebook-pixel")
-      .then((x) => x.default)
-      .then((ReactPixel) => {
-        ReactPixel.init(process.env.META_PIXEL_ID!); //don't forget to change this
-        ReactPixel.pageView();
-      });
-  }, [pathname, searchParams]);
-
-  return null;
+const FacebookPixel: React.FC = () => {
+  return (
+    <>
+      <Script
+        id="facebook-pixel"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1026568032297398');
+            fbq('track', 'PageView');
+          `,
+        }}
+      />
+      <noscript>
+        <img
+          height="1"
+          width="1"
+          style={{ display: "none" }}
+          alt="meta-pixel"
+          src="https://www.facebook.com/tr?id=1026568032297398&ev=PageView&noscript=1"
+        />
+      </noscript>
+    </>
+  );
 };
 
-export default MetaPixel;
+export default FacebookPixel;
