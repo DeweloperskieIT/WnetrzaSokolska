69c58ea42800b00798ff6e362377dfa26af75a85
KrzysiekDeweloperskie <krzysiek.kutniowski@deweloperskie.pl>
2024-08-20 07:52:26 +0200

    placeholder favicons and robots sitemap

--- app/layout.tsx
+++ app/layout.tsx
@@ -59,6 +59,20 @@ export const metadata: Metadata = {
       "Zamieszkaj w sercu Katowic w luksusowym apartamencie z unikalnym designem. Poznaj naszą ofertę.",
     images: ["https://wnetrza.deweloperskie.pl/images/mainpage/dszare.png"],
   },
+  icons: {
+    icon: [
+      { url: "/favicon/favicon-16x16.png", sizes: "16x16" },
+      { url: "/favicon/favicon-32x32.png", sizes: "32x32" },
+      { url: "/favicon/favicon.ico", sizes: "any" },
+    ],
+    apple: "/favicon/apple-touch-icon.png",
+    other: [
+      {
+        rel: "manifest",
+        url: "/favicon/site.webmanifest",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
