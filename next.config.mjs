88af27630519dca1af92673dff83fc64b9025b26
KrzysiekDeweloperskie <krzysiek.kutniowski@deweloperskie.pl>
2024-08-22 13:50:25 +0200

    enable csp back

--- next.config.mjs
+++ next.config.mjs
@@ -1,32 +1,32 @@
-// const cspHeader = `
-//     default-src 'self';
-//     script-src 'self' 'unsafe-eval' 'unsafe-inline' https://www.googletagmanager.com https://cdn.jsdelivr.net/gh/paulirish/lite-youtube-embed@master/src/;
-//     style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://cdn.jsdelivr.net/gh/paulirish/lite-youtube-embed@master/src/lite-yt-embed.css https://cdn.jsdelivr.net/gh/paulirish/lite-youtube-embed@master/src/ https://www.googletagmanager.com/;
-//     img-src 'self' https://i.ytimg.com/ https://www.googletagmanager.com/ blob: data:;
-//     font-src 'self' https://fonts.gstatic.com;
-//     object-src 'none';
-//     base-uri 'self';
-//     form-action 'self';
-//     frame-ancestors 'none';
-//     upgrade-insecure-requests;
-// `;
+const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'unsafe-eval' 'unsafe-inline' https://www.googletagmanager.com https://cdn.jsdelivr.net/gh/paulirish/lite-youtube-embed@master/src/;
+    style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://cdn.jsdelivr.net/gh/paulirish/lite-youtube-embed@master/src/lite-yt-embed.css https://cdn.jsdelivr.net/gh/paulirish/lite-youtube-embed@master/src/ https://www.googletagmanager.com/;
+    img-src 'self' https://i.ytimg.com/ https://www.googletagmanager.com/ blob: data:;
+    font-src 'self' https://fonts.gstatic.com;
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    upgrade-insecure-requests;
+`;
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
-  // async headers() {
-  //   return [
-  //     {
-  //       source: "/(.*)",
-  //       headers: [
-  //         {
-  //           key: "Content-Security-Policy",
-  //           value: cspHeader.replace(/\n/g, ""),
-  //         },
-  //       ],
-  //     },
-  //   ];
-  // },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: cspHeader.replace(/\n/g, ""),
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
