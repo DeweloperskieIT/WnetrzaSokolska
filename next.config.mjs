05e8ab4861fa92fb8d8c848d06fbcfdc21df6786
KrzysiekDeweloperskie <krzysiek.kutniowski@deweloperskie.pl>
2024-08-22 14:21:29 +0200

    fix vercel csp header

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 const cspHeader = `
     default-src 'self' ;
-    script-src 'self' 'unsafe-eval' 'unsafe-inline' 192.168.10.27 https://www.googletagmanager.com https://cdn.jsdelivr.net/gh/paulirish/lite-youtube-embed@master/src/;
+    script-src 'self' 'unsafe-eval' 'unsafe-inline' 192.168.10.27 https://www.googletagmanager.com https://cdn.jsdelivr.net/gh/paulirish/lite-youtube-embed@master/src/ https://va.vercel-scripts.com/v1/speed-insights/script.debug.js;
     style-src 'self' 'unsafe-inline'  https://fonts.googleapis.com https://cdn.jsdelivr.net/gh/paulirish/lite-youtube-embed@master/src/ https://www.googletagmanager.com/;
     img-src 'self'  https://i.ytimg.com/ https://www.googletagmanager.com/ blob: data:;
     font-src 'self'  https://fonts.gstatic.com;
